ab064bf09a5a105d0196ecd78ac236fbb5713e00
xiech <xiechanghong@everimaging.com>
2019-01-21 17:42:43 +0800

    增加启动成功提示!

--- main.py
+++ main.py
@@ -2,11 +2,14 @@
 # encoding: utf-8
 
 from dht import DHTServer
-#from model.init import database_initialize
 from common.database import RedisClients
+from common.utils import get_logger
+from config import Config
+
+logger = get_logger("logger_dht_main")
 
 if __name__ == "__main__":
-    #RedisClients.set_keyinfo("4d8ea85ce90562156ebd7330bd631eaa7b6825f0","1111111")
     dht = DHTServer()
     dht.start()
     dht.auto_send_find_node()
+    logger.info("dht running successful ! >>>> {0}:{1}".format(Config.BIND_IP,Config.BIND_PORT))
