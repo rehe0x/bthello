ad358ec854ee92683e10f843371c60d38433fe1b
xiech <xiechanghong@everimaging.com>
2019-01-23 21:47:17 +0800

    线程优化

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ from multiprocessing import cpu_count
 
 class Config(object):
     # 是否使用全部进程
-    MAX_PROCESSES = cpu_count() // 2 or cpu_count()
+    MAX_PROCESSES = 2
 
     #############node节点#####################
     BOOTSTRAP_NODES = (
